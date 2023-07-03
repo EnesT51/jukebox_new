ae6ac75f928e466f94b2b88d0cfcd8bc442b9efa
EnesT51 <Enes.T51@outlook.com>
2023-07-03 21:04:58 +0200

    song details

---
+++ resources/views/song/detail.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Song detail</title>
+</head>
+<body>
+    <ul>
+        <h1>Song: {{$detail->name}}</h1>
+        <p>Author:{{$detail->author}} </p>
+        <p>Releasedate: {{$detail->releasedate}}</p>
+        <p>Duration: {{$detail->duration}}</p>
+        <p>Genre: {{$detail->genre}}</p>
+    </ul>
+</body>
+</html>
