bfdcf85410d5937ada534b7075517c108a2cd224
EnesT51 <Enes.T51@outlook.com>
2023-09-11 18:12:20 +0200

    added update and edit routs for songs and playlist

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/master');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard', function () { 
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -32,29 +32,46 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/genre/all', [GenreController::class, 'index'])->name('genre.index');
-Route::get('/genre/create', [GenreController::class, 'create'])->name('genre.create');
-Route::post('/genre/store', [GenreController::class, 'store'])->name('genre.store');
-Route::get('/genre/destroy/{genre}', [GenreController::class, 'destroy'])->name('genre.destroy');
+Route::middleware(['auth', 'disable-back-btn'])->group(function() {
 
-Route::get('/song/all', [SongController::class, 'index'])->name('song.index');
-Route::post('add-songs-to-playlist', [SongController::class, 'addSongsToPlaylist'])->name('add-song-to-playlist');
-Route::post('remove-song-from-playlist', [SongController::class, 'RemoveSongFromPlaylist'])->name('remove-song-from-playlist');
-Route::get('/song/detail/{id}', [SongController::class, 'SongDetail'])->name('song.detail');
-Route::get('/song/create', [SongController::class, 'create'])->name('song.create');
-Route::post('/song/store', [SongController::class, 'store'])->name('song.store');
-Route::get('/song/destroy/{song}', [SongController::class, 'destroy'])->name('song.destroy');
+    Route::get('/genre/all', [GenreController::class, 'index'])->name('genre.index');
+    Route::get('/genre/create', [GenreController::class, 'create'])->name('genre.create');
+    Route::post('/genre/store', [GenreController::class, 'store'])->name('genre.store');
+    Route::get('/genre/destroy/{genre}', [GenreController::class, 'destroy'])->name('genre.destroy');
+    
+    Route::get('/song/{song}/edit', [SongController::class, 'edit'])->name('song.edit');
+    Route::get('/song/{song}/update', [SongController::class, 'update'])->name('song.update');
+    Route::get('/song/all', [SongController::class, 'index'])->name('song.index');
+    Route::post('add-songs-to-playlist', [SongController::class, 'addSongsToPlaylist'])->name('add-song-to-playlist');
+    Route::post('remove-song-from-playlist', [SongController::class, 'RemoveSongFromPlaylist'])->name('remove-song-from-playlist');
+    Route::get('/song/detail/{id}', [SongController::class, 'SongDetail'])->name('song.detail');
+    Route::get('/song/create', [SongController::class, 'create'])->name('song.create');
+    Route::post('/song/store', [SongController::class, 'store'])->name('song.store');
+    Route::get('/song/destroy/{song}', [SongController::class, 'destroy'])->name('song.destroy');
 
-Route::get('/playlist/all', [PlaylistController::class, 'index'])->name('playlist.index');
-Route::get('/playlist/create', [PlaylistController::class, 'create'])->name('playlist.create');
-Route::post('/playlist/store', [PlaylistController::class, 'store'])->name('playlist.store');
-Route::get('/playlist/destroy/{playlist}', [PlaylistController::class, 'destroy'])->name('playlist.destroy');
-Route::get('/playlist/show/{id}', [PlaylistController::class, 'show'])->name('playlist.show');
+    Route::get('/playlist/all', [PlaylistController::class, 'index'])->name('playlist.index');
+    Route::get('/playlist/create', [PlaylistController::class, 'create'])->name('playlist.create');
+    Route::get('/playlist/{playlist}/update', [PlaylistController::class, 'update'])->name('playlist.update');
+    Route::get('/playlist/{playlist}/edit', [PlaylistController::class, 'edit'])->name('playlist.edit');
 
+    Route::post('/playlist/store', [PlaylistController::class, 'store'])->name('playlist.store');
+    Route::get('/playlist/destroy/{playlist}', [PlaylistController::class, 'destroy'])->name('playlist.destroy');
+    Route::get('/playlist/show/{id}', [PlaylistController::class, 'show'])->name('playlist.show');
 
+});
 
 require __DIR__.'/auth.php';
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+
+
+
+
+
+
+
